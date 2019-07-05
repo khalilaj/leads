0ad75fd30fb6c2e543594e803eadb992f8205b79
Aj Khalil <khalilajstunner@gmail.com>
2019-07-05 21:34:18 +0300

    react implemented

---
+++ frontend/src/components/App.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+import ReactDOM from "react-dom";
+import "./App.css";
+
+class App extends Component {
+  render() {
+    return <h1>HEY AJ FOR THE LAST TIME</h1>;
+  }
+}
+
+ReactDOM.render(<App />, document.getElementById("app"));
