f31b6c8404a7e9a8a2bb1b3aa0312ed9b7634874
Aj Khalil <khalilajstunner@gmail.com>
2019-07-08 13:38:59 +0300

    backendauth

--- leadmanager/settings.py
+++ leadmanager/settings.py
@@ -39,8 +39,14 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'frontend',
-    'rest_framework'
+    'rest_framework',
+    'knox',
+    'accounts',
 ]
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': ('knox.auth.TokenAuthentication',)
+}
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
